adc62173cd76de10d191a51dc52de639c37764e9
alvarantson <alvarantson@gmail.com>
2019-11-06 20:15:29 +0200

    static to media fix

--- LEHT/urls.py
+++ LEHT/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -37,4 +39,4 @@ urlpatterns = [
         ),
         name="favicon" # name of our view
     ),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
