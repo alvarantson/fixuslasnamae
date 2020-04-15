90cb049682c42a3e55dc0feaa03fdcd353d4ac25
alvarantson <alvarantson@gmail.com>
2020-04-15 21:41:07 +0300

    ihatemyself update (XML converter)

--- xmltotxt/views.py
+++ xmltotxt/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 from login.models import worker
 from .forms import UploadFileForm
-from .XML_TXT02 import *
+from .conv import *
 from django.http import HttpResponse, Http404
+from login.views import is_worker
 # Create your views here.
 
 def xmltotxt(request):
-	try:
-		request.session['kalender_priority'] = worker.objects.get(name=request.session['worker']).kalender_priority
-		request.session['varuosad_priority'] = worker.objects.get(name=request.session['worker']).varuosad_priority
-		request.session['tookoda_priority'] = worker.objects.get(name=request.session['worker']).tookoda_priority
-	except:
-		return HttpResponseRedirect('/login') #INDEX\i puhul '/'
+	is_worker(request)
 	if request.method == 'POST':
 		form = UploadFileForm(request.POST, request.FILES)
-		file_data = writeTXT(ImportXML(request.FILES["file"]), request.POST["name"])
-		response = HttpResponse(file_data, content_type='application/text charset=utf-8')
-		response['Content-Disposition'] = 'attachment; filename="{}.txt"'.format(request.POST["name"])
+		inquiry = import_motoral(request.FILES["file"])
+		doc = export_baltiautoosad(inquiry)
+
+#		file_data = writeTXT(ImportXML(), request.POST["name"])
+		response = HttpResponse(doc.toxml(encoding="utf-8"), content_type='application/text charset=utf-8')
+		response['Content-Disposition'] = 'attachment; filename="{}.xml"'.format(request.POST["name"])
 		return response
 	else:
 		form = UploadFileForm()
