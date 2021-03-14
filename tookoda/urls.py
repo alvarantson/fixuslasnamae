4b3b8b56df4576d385b30792698f8b0a3aaed495
alvarantson <47379716+alvarantson@users.noreply.github.com>
2021-03-14 16:01:03 +0200

    fix2
    fix2

--- tookoda/urls.py
+++ tookoda/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("hooldus/", views.hooldus),
-    path("rehvivahetus/", views.rehvivahetus)
+    path("/hooldus", views.hooldus),
+    path("/rehvivahetus", views.rehvivahetus)
 
 ]
